8c0f4db7e5b5d761e63f87afe708d4a1a33b8889
TweenzY-Y <tweenzy.mail@gmail.com>
2025-08-22 23:18:27 +0200

    My first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Lesson from "./components/Lesson";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <Lesson header="Your first component">
+      <p>
+        Components are used to divide website for reusable blocks. That design pattern also changes the way of
+        implementing actual website design into code. React components can be mixed with styles and
+        <strong> JavaScript</strong> logic in one place.
       </p>
-    </>
-  )
+      <h3>Defining a component</h3>
+      <p>Defining a component can be described by 3 steps</p>
+      <ol>
+        <li>Exporting component</li>
+        <li>Defining a component as a JavaScript function</li>
+        <li>Adding a JSX markup as a return of a function</li>
+      </ol>
+      <p>
+        I can keep multiple components in one file when they are strongly related to each other and small enough. I can
+        always move component into separate file.
+      </p>
+      <p>I should never nest component declarations! All component declarations should be at top level.</p>
+    </Lesson>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Lesson.jsx
@@ -0,0 +1,8 @@
+export default function Lesson({ header, children }) {
+  return (
+    <>
+      <h2>{header}</h2>
+      <p>{children}</p>
+    </>
+  );
+}
