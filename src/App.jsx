3118a4d9c9247ddcc54978bc91efca907d066f58
TweenzY-Y <tweenzy.mail@gmail.com>
2025-08-23 12:24:32 +0200

    Importing and exporting components & Writing markup with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,42 @@ import Lesson from "./components/Lesson";
 
 function App() {
   return (
-    <Lesson header="Your first component">
-      <p>
-        Components are used to divide website for reusable blocks. That design pattern also changes the way of
-        implementing actual website design into code. React components can be mixed with styles and
-        <strong> JavaScript</strong> logic in one place.
-      </p>
-      <h3>Defining a component</h3>
-      <p>Defining a component can be described by 3 steps</p>
-      <ol>
-        <li>Exporting component</li>
-        <li>Defining a component as a JavaScript function</li>
-        <li>Adding a JSX markup as a return of a function</li>
-      </ol>
-      <p>
-        I can keep multiple components in one file when they are strongly related to each other and small enough. I can
-        always move component into separate file.
-      </p>
-      <p>I should never nest component declarations! All component declarations should be at top level.</p>
-    </Lesson>
+    <>
+      <Lesson header="Your first component">
+        <p>
+          Components are used to divide website for reusable blocks. That design pattern also changes the way of
+          implementing actual website design into code. React components can be mixed with styles and
+          <strong> JavaScript</strong> logic in one place.
+        </p>
+        <h3>Defining a component</h3>
+        <p>Defining a component can be described by 3 steps</p>
+        <ol>
+          <li>Exporting component</li>
+          <li>Defining a component as a JavaScript function</li>
+          <li>Adding a JSX markup as a return of a function</li>
+        </ol>
+        <p>
+          I can keep multiple components in one file when they are strongly related to each other and small enough. I
+          can always move component into separate file.
+        </p>
+        <p>I should never nest component declarations! All component declarations should be at top level.</p>
+      </Lesson>
+      <Lesson header="Importing and exporting components">
+        <p>I can export components by default export or by named export.</p>
+        <p>
+          Most dev teams stick with one type of exporting components to avoid confusion. I'd stick with default
+          exporting. I started with that and it feels natural for me.
+        </p>
+      </Lesson>
+      <Lesson header="Writing markup with JSX">
+        <p>Rules of JSX:</p>
+        <ol>
+          <li>Must return a single root element</li>
+          <li>Close all tags!</li>
+          <li>Most of attributes are camelCase</li>
+        </ol>
+      </Lesson>
+    </>
   );
 }
 
